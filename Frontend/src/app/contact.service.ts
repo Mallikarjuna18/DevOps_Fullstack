cc8846cc892657312fb2cc84d09c412b69eccd64
mallikarjuna kukatikonda <[200~>
2023-08-25 10:33:23 +0530

    first

---
+++ Frontend/src/app/contact.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Contact } from './contact';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  private baseUrl="http://a2dd8f4fe4265469c97db8fb4f68d5fa-606287383.ap-south-1.elb.amazonaws.com"
+
+  constructor(private httpClient:HttpClient) { }
+
+  createContact(contact:Contact):Observable<string>{
+    return this.httpClient.post(`${this.baseUrl}/contact`, contact, { responseType:"text" } );
+  }
+
+  getContacts():Observable<Contact[]>{
+    return this.httpClient.get<Contact[]>(`${this.baseUrl}/contacts`);
+  }
+
+  findContactById(id:number):Observable<Contact>{
+    return this.httpClient.get<Contact>(`${this.baseUrl}/contact/${id}`);
+  }
+
+  deleteContactByid(id:number):Observable<string>{
+    return this.httpClient.delete(`${this.baseUrl}/contact/${id}`, {responseType:"text"});
+  }
+
+}
